d73d225f4291b83c674d77c22c71f1aceb9c3b55
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-02-22 23:32:25 +0100

    SDI-2021-805-3.4-Autenticación y control de acceso.

--- sdi2021-805-spring/src/main/java/com/uniovi/repositories/UsersRepository.java
+++ sdi2021-805-spring/src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -4,5 +4,6 @@ import com.uniovi.entities.*;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
+	User findByDni(String dni);
 	
 }
