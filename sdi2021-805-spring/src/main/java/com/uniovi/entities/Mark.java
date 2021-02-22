86e48ee6a1bab38b0b4835ddb94c4427e75fd91d
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-02-22 23:13:14 +0100

    SDI-2021-805-3.3-Acceso a datos con entidades relacionadas

--- sdi2021-805-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi2021-805-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -3,6 +3,8 @@ package com.uniovi.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Mark {
@@ -12,6 +14,10 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
@@ -19,9 +25,17 @@ public class Mark {
 		this.score = score;
 	}
 
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
+	}
+
 	public Mark() {
 	}
 
+	
 	@Override
 	public String toString() {
 		return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
@@ -49,5 +63,13 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 }
