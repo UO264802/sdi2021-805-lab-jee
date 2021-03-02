1ce552d52bbecc80564b5042c7f983ef0e133f12
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-03-02 12:28:45 +0100

    SDI-2021-805-Sesión4-Ejercicio-complementario1

--- sdi2021-805-spring/src/main/java/com/uniovi/WebSecurityConfig.java
+++ sdi2021-805-spring/src/main/java/com/uniovi/WebSecurityConfig.java
@@ -35,8 +35,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/mark/add").hasAuthority("ROLE_PROFESSOR").antMatchers("/mark/edit/*")
 				.hasAuthority("ROLE_PROFESSOR").antMatchers("/mark/delete/*").hasAuthority("-ROLE_PROFESSOR")
 				.antMatchers("/mark/**").hasAnyAuthority("ROLE_STUDENT", "ROLE_PROFESSOR", "ROLE_ADMIN")
-				.antMatchers("/user/**").hasAnyAuthority("ROLE_ADMIN").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().defaultSuccessUrl("/home").and().logout().permitAll();
+				.antMatchers("/user/**").hasAnyAuthority("ROLE_ADMIN").anyRequest().authenticated().and()
+				.formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.defaultSuccessUrl("/home")
+					.and()
+				.logout()
+					.permitAll();
 	}
 
 	@Autowired
