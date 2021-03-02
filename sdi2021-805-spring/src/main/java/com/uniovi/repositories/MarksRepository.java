21958f366a6fe61199089952a8f7c6d68d185dea
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-03-02 02:42:24 +0100

    SDI-2021-805-4.6-Paginación.

--- sdi2021-805-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
+++ sdi2021-805-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -1,9 +1,9 @@
 package com.uniovi.repositories;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,13 +13,15 @@ import com.uniovi.entities.User;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
 	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1)" + "OR LOWER(r.user.name) LIKE LOWER(?1))")
-	List<Mark> searchByDescriptionAndName(String searchText);
+	Page<Mark> searchByDescriptionAndName(Pageable pageable, String searchText);
 
 	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1)"
 			+ "OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2 ")
-	List<Mark> searchByDescriptionNameAndUser(String searchText, User user);
+	Page<Mark> searchByDescriptionNameAndUser(Pageable pageable, String searchText, User user);
 
-	List<Mark> findAllByUser(User user);
+	Page<Mark> findAllByUser(Pageable pageable, User user);
+	
+	Page<Mark> findAll(Pageable pageable);
 
 	@Modifying
 	@Transactional
